30a0b01d18b3ab2340533abb367bf2373135799c
lucasbogo <lucasbogo93@gmail.com>
2022-03-28 10:46:01 -0300

    needs work

--- src/main/java/br/com/cineclube/model/Movie.java
+++ src/main/java/br/com/cineclube/model/Movie.java
@@ -1,10 +1,11 @@
 package br.com.cineclube.model;
 
 import java.math.BigDecimal;
+import java.sql.Date;
 
 //import javax.persistence.Entity;
 
-//@Entity
+//@Entity CONSUMIR APIs NÃO ENVOLVE BD LOCAL...
 public class Movie {
 	
 	private Long id;
@@ -12,6 +13,7 @@ public class Movie {
 	private String overview;
 	private Integer vote_count;
 	private BigDecimal vote_average;
+	private Date release_date;
 	
 	
 	//getters and setters
@@ -44,6 +46,12 @@ public class Movie {
 	}
 	public void setVote_average(BigDecimal vote_average) {
 		this.vote_average = vote_average;
+	}
+	public Date getRelease_date() {
+		return release_date;
+	}
+	public void setRelease_date(Date release_date) {
+		this.release_date = release_date;
 	}
 	
 
